0e59fe67a86a27ca8a983a41e092b5b54cccca8a
steve <mainastephen159@gmail.com>
2025-07-07 15:16:07 +0300

    admin updates

--- LibraryProject/bookshelf/admin.py
+++ LibraryProject/bookshelf/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Displayed columns
+    list_filter = ('publication_year',)  # Sidebar filter
+    search_fields = ('title', 'author')  # Search bar
